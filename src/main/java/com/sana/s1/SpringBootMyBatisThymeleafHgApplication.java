072ae8e8e0148013e69bee12aa46eb9852d83651
gjrmsp <gjrmsp@naver.com>
2021-05-13 16:13:20 +0900

    Initial commit

---
+++ src/main/java/com/sana/s1/SpringBootMyBatisThymeleafHgApplication.java
@@ -0,0 +1,13 @@
+package com.sana.s1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMyBatisThymeleafHgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMyBatisThymeleafHgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sana/s1/SpringBootMyBatisThymeleafHgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sana.s1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMyBatisThymeleafHgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
